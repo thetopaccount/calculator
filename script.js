caa4b333cf3fb8d8b9edfb2500a20dbfceb954cb
thetopaccount <thetopaccount@protonmail.com>
2022-08-11 18:26:21 +0530

    Define logic for handling button inputs

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function checkIfValidNumbers(...chars) {
 
 const operate = (operator, a, b) => {
 	try {
-		if (!checkIfValidNumbers(a, b) || !['+', '-', '*', '/'].includes(operator)) {
+		if (!checkIfValidNumbers(a, b) || !(/[\+\-\*\/]/).test(operator)) {
 			throw new Error('Invalid operation');
 		}
 		switch (operator) {
@@ -29,4 +29,86 @@ const operate = (operator, a, b) => {
 	} catch (e) {
 		console.error(`${e.name}: ${e.message}`);
 	}
+}
+
+const calculateResult = () => {
+	try {
+		// throw new Error('Error while calculating result');
+		return 'Logic WIP';
+	} catch (e) {
+		console.error(`${e.name}: ${e.message}`);
+	}
+}
+
+let numberStack = [];
+let operatorStack = [];
+let currentNum = '';
+
+const calcDigits = document.getElementById('calc-digits');
+const calcScreen = document.getElementById('calc-screen');
+const calcOperators = document.getElementById('calc-operators');
+
+calcDigits.addEventListener('click', (e) => {
+	calcScreen.value += e.target.textContent;
+	currentNum += e.target.textContent;
+});
+
+calcOperators.addEventListener('click', (e) => {
+	handleEnteredOperator(e.target.textContent);
+});
+
+const handleEnteredOperator = (enteredOperator) => {
+	switch (enteredOperator) {
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+			// expression cannot start with operators
+			if (calcScreen.value?.length === 0 && !calcScreen.value) break;
+			// prevent entry of successive operators
+			if (hasSuccessiveOperators(calcScreen.value?.slice?.(-1) + enteredOperator)) {
+				calcScreen.value = calcScreen.value?.slice?.(0, -1);
+				operatorStack.pop();
+			} else {
+				numberStack.push(currentNum);
+			}
+			calcScreen.value += enteredOperator;
+			operatorStack.push(enteredOperator);
+			currentNum = '';
+			break;
+		case 'C':
+			calcScreen.value = '';
+			resetCalcOperations();
+			break;
+		case '⌫':
+			if ((/[\+\-\*\/]/).test(calcScreen.value?.slice?.(-1))) {
+				operatorStack.pop();
+				currentNum = numberStack.pop();
+			} else {
+				currentNum = currentNum?.slice?.(0, -1);
+			}
+			calcScreen.value = calcScreen.value?.slice?.(0, -1);
+			break;
+		case '=':
+			if (currentNum) {
+				numberStack.push(currentNum);
+			}
+			calcScreen.value = calculateResult();
+			resetCalcOperations();
+			break;
+		default	:
+			break;
+	}
+}
+
+const resetCalcOperations = () => {
+	currentNum = '';
+	numberStack = [];
+	operatorStack = [];
+}
+
+const hasSuccessiveOperators = (string) => (/[\+\-\*\/]{2}/).test(string);
+
+window.onload = () => {
+	calcScreen.value = '';
 }
